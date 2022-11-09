fef024e85a2dca92de284baf39743407c4e18170
rafa2996378727@gmail.com <rafa2996378727@gmail.com>
2022-11-09 20:54:02 -0300

    a

---
+++ src/components/Estatus.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+// El componente Estatus no tiene componentes hijos.
+// ESTADO: Estatus no tiene estado.
+// MÉTODOS: Estatus no tiene métodos.
+// PROPS: Estatus recibe de su padre la cantidad que va a mostrar dentro del span correpondiente.
+// Maqueta de Estatus:
+//    h1
+//    p > span     (el span mostrará la cantidad recibida por props)
+
+export default function Estatus() {
+  
+  return (
+    <header>
+        {/* maquetar Estatus aquí */}
+        
+         <h1></h1>
+         
+          <p> <span>    </span></p>
+        
+
+    </header>
+  )
+}
